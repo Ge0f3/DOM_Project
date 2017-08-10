6ca6e71e6859054a8a2aa889c503efddc41b9992
Geoffrey <geo.geofe@gmail.com>
2017-08-10 12:25:22 -0500

    Finished with all the fn

--- script.js
+++ script.js
@@ -2,23 +2,41 @@ var numberOfFaces = 4;
 
 function createIMG() {
     var leftside = document.getElementById('leftside');
-    for (var i = 0; i < numberOfFaces; i++) {
-        createmin(leftside);
-    }
     var rightside = document.getElementById('rightside');
+    var container = document.getElementById('container');
+    var div = document.getElementsByTagName('div');
+    while(leftside.firstChild){
+    	leftside.removeChild(leftside.firstChild);
+    }
+	while(rightside.firstChild){
+    	rightside.removeChild(rightside.firstChild);
+    }
+
+    for (var i = 0; i < numberOfFaces; i++) {
+        createmin();
+    }
+    
     leftsideIMGs = leftside.cloneNode(true);
     leftsideIMGs.removeChild(leftsideIMGs.lastChild);
     rightside.append(leftsideIMGs);
     leftside.lastChild.onclick = function nextLevel(event) {
         event.stopPropagation();
-        numberOfFaces += 3;
+        numberOfFaces += 2;
         createIMG();
 
     }
+    leftside.onclick=function gameover(event) {
+
+    	 alert("Game Over!");
+
+    leftside.lastChild.onclick = null;
+    	// body...
+    }
+
      // body...
 }
-function createmin(leftside) {
-    var leftside = leftside;
+function createmin() {
+    var leftside = document.getElementById('leftside');
     var img = document.createElement("img");
     img.src = "minion.png"
     img.style.top = ((Math.floor((Math.random() * 400) + 1)).toString()) + "px";
@@ -27,5 +45,9 @@ function createmin(leftside) {
 
     // body...
 }
-
+window.onload = function(){
+    document.getElementsByTagName('body').onclick=function(){
+        alert("Hello WOrld");
+    }
+}
 
