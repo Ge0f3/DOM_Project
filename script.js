0ea43e5fd5940e9c17a7c14dc0b3b77ec3676803
Geoffrey <geo.geofe@gmail.com>
2017-08-10 04:34:51 -0500

    Done multiplying faces

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
+var numberOfFaces = 4;
 
 function createIMG() {
-	var leftside=document.getElementById('leftside')
-	for (var i = 0; i < 6; i++) {
-		createmin();
-	}
-	var rightside=document.getElementById('rightside');
-	leftsideIMGs=leftside.cloneNode(true);
-	leftsideIMGs.removeChild(leftsideIMGs.lastChild);
-	rightside.append(leftsideIMGs);
-	// body...
+    var leftside = document.getElementById('leftside');
+    for (var i = 0; i < numberOfFaces; i++) {
+        createmin(leftside);
+    }
+    var rightside = document.getElementById('rightside');
+    leftsideIMGs = leftside.cloneNode(true);
+    leftsideIMGs.removeChild(leftsideIMGs.lastChild);
+    rightside.append(leftsideIMGs);
+    leftside.lastChild.onclick = function nextLevel(event) {
+        event.stopPropagation();
+        numberOfFaces += 3;
+        createIMG();
+
+    }
+     // body...
+}
+function createmin(leftside) {
+    var leftside = leftside;
+    var img = document.createElement("img");
+    img.src = "minion.png"
+    img.style.top = ((Math.floor((Math.random() * 400) + 1)).toString()) + "px";
+    img.style.left = ((Math.floor((Math.random() * 400) + 1)).toString()) + "px";
+    leftside.append(img);
+
+    // body...
 }
 
-function createmin() {
-	var leftside=document.getElementById('leftside')
-	var img= document.createElement("img");
-	img.src="minion.png"
-	img.style.top=((Math.floor((Math.random() * 400) + 1)).toString()) + "px";
-	img.style.left= ((Math.floor((Math.random() * 400) + 1)).toString()) + "px";
-	leftside.append(img);
 
-	// body...
-}
